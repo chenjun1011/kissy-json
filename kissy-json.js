9ef3e1084719b9197e5b4cf8be374e52a6fe2927
chenjun <chenjun_1011@hotmail.com>
2013-04-01 11:24:18 +0800

    first commit

---
+++ kissy-json.js
@@ -0,0 +1,71 @@
+/**
+ * @author shuilan
+ */
+var kissy={
+	"seed":{
+		"lang":{
+			//第一种方式 按现有文档罗列
+			"augment":{},
+			"available":{},
+			"bind":{},
+			"buffer":{},
+			"clone":{},
+			"each":{},
+			"endsWith":{},
+			"error":{},
+			"escapeHTML":{},
+			"extend":{},
+			"inArray":{},
+			"lastIndexOf":{},
+			//第二种方式，把属于array的方法放在一起
+			"array":{
+				"each":{},
+				"indexOf":{},
+				"lastIndexOf":{},
+				"unique":{},
+				"inArray":{},
+				"filter":{},
+				"map":{},
+				"reduce":{},
+				"every":{},
+				"some":{},
+				"makeArray":{}
+			}
+		},
+		"loader":{
+			
+		}
+	},
+	"core":{
+		"ajax":{
+			
+		},
+		"anim":{
+			
+		},
+		"base":{
+			
+		},
+		"cookie":{
+			
+		},
+		"dom":{
+			
+		},
+		"event":{
+			
+		},
+		"json":{
+			
+		},
+		"node":{
+			
+		},
+		"ua":{
+			
+		}
+	},
+	"component":{
+		
+	}
+};
